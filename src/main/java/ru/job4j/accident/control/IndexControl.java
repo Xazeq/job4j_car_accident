8449ea3dfa6107ea8961859e499ec1b16c79a95d
Xazeq <setezey@gmail.com>
2022-02-16 18:00:18 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Ivan Ivanov");
+        List<String> cities = List.of("Москва", "Санкт-Петербург", "Нижний Новгород", "Казань");
+        model.addAttribute("cities", cities);
         return "index";
     }
 }
